c62f8d6c974ee308cff15ba2bb4346b11dcec3cf
Jeremy Chaput <1964982@cstj.qc.ca>
2023-09-01 20:02:51 -0400

    Patch 0.0.1

--- Build/Builds.framework.js
+++ Build/Builds.framework.js
@@ -887,6 +887,17 @@ function allocateUTF8OnStack(str) {
  return ret;
 }
 
+function writeStringToMemory(string, buffer, dontAddNull) {
+ warnOnce("writeStringToMemory is deprecated and should not be called! Use stringToUTF8() instead!");
+ var lastChar, end;
+ if (dontAddNull) {
+  end = buffer + lengthBytesUTF8(string);
+  lastChar = HEAP8[end];
+ }
+ stringToUTF8(string, buffer, Infinity);
+ if (dontAddNull) HEAP8[end] = lastChar;
+}
+
 function writeArrayToMemory(array, buffer) {
  assert(array.length >= 0, "writeArrayToMemory array must have a length (should be an array or typed array)");
  HEAP8.set(array, buffer);
@@ -1270,29 +1281,29 @@ var tempDouble;
 var tempI64;
 
 var ASM_CONSTS = {
- 3948420: function() {
+ 3949332: function() {
   Module["emscripten_get_now_backup"] = performance.now;
  },
- 3948475: function($0) {
+ 3949387: function($0) {
   performance.now = function() {
    return $0;
   };
  },
- 3948523: function($0) {
+ 3949435: function($0) {
   performance.now = function() {
    return $0;
   };
  },
- 3948571: function() {
+ 3949483: function() {
   performance.now = Module["emscripten_get_now_backup"];
  },
- 3948626: function() {
+ 3949538: function() {
   return Module.webglContextAttributes.premultipliedAlpha;
  },
- 3948687: function() {
+ 3949599: function() {
   return Module.webglContextAttributes.preserveDrawingBuffer;
  },
- 3948751: function() {
+ 3949663: function() {
   return Module.webglContextAttributes.powerPreference;
  }
 };
@@ -1429,6 +1440,46 @@ function _GetJSMemoryInfo(totalJSptr, usedJSptr) {
   HEAPF64[totalJSptr >> 3] = NaN;
   HEAPF64[usedJSptr >> 3] = NaN;
  }
+}
+
+function _GetOverlayHtmlInputFieldValue() {
+ var inputField = document.getElementById("nativeInputDialogInput");
+ var result = "";
+ if (inputField && inputField.value) {
+  result = inputField.value;
+ }
+ var buffer = _malloc(lengthBytesUTF8(result) + 1);
+ writeStringToMemory(result, buffer);
+ return buffer;
+}
+
+function _HideUnityScreenIfHtmlOverlayCant() {
+ if (navigator.userAgent.indexOf("Chrome/") < 0) {
+  document.getElementById("canvas").style.display = "none";
+ }
+}
+
+function _IsOverlayDialogHtmlActive() {
+ var nativeDialog = document.getElementById("nativeInputDialog");
+ if (!nativeDialog) {
+  return false;
+ }
+ return nativeDialog.style.display != "none";
+}
+
+function _IsOverlayDialogHtmlCanceled() {
+ var check = document.getElementById("nativeInputDialogCheck");
+ if (!check) {
+  return false;
+ }
+ return check.checked;
+}
+
+function _IsRunningOnEdgeBrowser() {
+ if (navigator.userAgent.indexOf("Edge/") < 0) {
+  return false;
+ }
+ return true;
 }
 
 var JS_Accelerometer = null;
@@ -3036,6 +3087,51 @@ function _JS_SystemInfo_HasWebGL() {
 
 function _JS_UnityEngineShouldQuit() {
  return !!Module.shouldQuit;
+}
+
+function _NativeDialogPrompt(title, defaultValue) {
+ defaultValue = Pointer_stringify(defaultValue);
+ title = Pointer_stringify(title);
+ var result = window.prompt(title, defaultValue);
+ if (!result) {
+  result = defaultValue;
+ }
+ var buffer = _malloc(lengthBytesUTF8(result) + 1);
+ writeStringToMemory(result, buffer);
+ return buffer;
+}
+
+function _SetupOverlayDialogHtml(title, defaultValue, okBtnText, cancelBtnText) {
+ title = Pointer_stringify(title);
+ defaultValue = Pointer_stringify(defaultValue);
+ okBtnText = Pointer_stringify(okBtnText);
+ cancelBtnText = Pointer_stringify(cancelBtnText);
+ if (!document.getElementById("nativeInputDialogInput")) {
+  var style = document.createElement("style");
+  style.setAttribute("id", "inputDialogTextSelect");
+  style.appendChild(document.createTextNode("#nativeInputDialogInput::-moz-selection { background-color:#00ffff;}"));
+  style.appendChild(document.createTextNode("#nativeInputDialogInput::selection { background-color:#00ffff;}"));
+  document.head.appendChild(style);
+ }
+ if (!document.getElementById("nativeInputDialog")) {
+  var html = '<div id="nativeInputDialog" style="background:#000000;opacity:0.9;width:100%;height:100%;position:fixed;top:0%;z-index:2147483647;">' + '  <div style="position:relative;top:30%;" align="center" vertical-align="middle">' + '    <div id="nativeInputDialogTitle" style="color:#ffffff;">Here is title</div>' + "    <div>" + '      <input id="nativeInputDialogInput" type="text" size="40" onsubmit="">' + "    </div>" + '    <div style="margin-top:10px">' + '      <input id="nativeInputDialogOkBtn" type="button" value="OK" onclick="" >' + '      <input id="nativeInputDialogCancelBtn" type="button" value="Cancel" onclick ="">' + '      <input id="nativeInputDialogCheck" type="checkBox" style="display:none;">' + "    </div>" + "  </div>" + "</div>";
+  var element = document.createElement("div");
+  element.innerHTML = html;
+  document.body.appendChild(element);
+  var okFunction = 'document.getElementById("nativeInputDialog" ).style.display = "none";' + 'document.getElementById("nativeInputDialogCheck").checked = false;' + 'document.getElementById("canvas").style.display="";';
+  var cancelFunction = 'document.getElementById("nativeInputDialog" ).style.display = "none";' + 'document.getElementById("nativeInputDialogCheck").checked = true;' + 'document.getElementById("canvas").style.display="";';
+  var inputField = document.getElementById("nativeInputDialogInput");
+  inputField.setAttribute("onsubmit", okFunction);
+  var okBtn = document.getElementById("nativeInputDialogOkBtn");
+  okBtn.setAttribute("onclick", okFunction);
+  var cancelBtn = document.getElementById("nativeInputDialogCancelBtn");
+  cancelBtn.setAttribute("onclick", cancelFunction);
+ }
+ document.getElementById("nativeInputDialogTitle").innerText = title;
+ document.getElementById("nativeInputDialogInput").value = defaultValue;
+ document.getElementById("nativeInputDialogOkBtn").value = okBtnText;
+ document.getElementById("nativeInputDialogCancelBtn").value = cancelBtnText;
+ document.getElementById("nativeInputDialog").style.display = "";
 }
 
 function ___assert_fail(condition, filename, line, func) {
@@ -13119,6 +13215,11 @@ function checkIncomingModuleAPI() {
 
 var asmLibraryArg = {
  "GetJSMemoryInfo": _GetJSMemoryInfo,
+ "GetOverlayHtmlInputFieldValue": _GetOverlayHtmlInputFieldValue,
+ "HideUnityScreenIfHtmlOverlayCant": _HideUnityScreenIfHtmlOverlayCant,
+ "IsOverlayDialogHtmlActive": _IsOverlayDialogHtmlActive,
+ "IsOverlayDialogHtmlCanceled": _IsOverlayDialogHtmlCanceled,
+ "IsRunningOnEdgeBrowser": _IsRunningOnEdgeBrowser,
  "JS_Accelerometer_IsRunning": _JS_Accelerometer_IsRunning,
  "JS_Accelerometer_Start": _JS_Accelerometer_Start,
  "JS_Accelerometer_Stop": _JS_Accelerometer_Stop,
@@ -13191,6 +13292,8 @@ var asmLibraryArg = {
  "JS_SystemInfo_HasFullscreen": _JS_SystemInfo_HasFullscreen,
  "JS_SystemInfo_HasWebGL": _JS_SystemInfo_HasWebGL,
  "JS_UnityEngineShouldQuit": _JS_UnityEngineShouldQuit,
+ "NativeDialogPrompt": _NativeDialogPrompt,
+ "SetupOverlayDialogHtml": _SetupOverlayDialogHtml,
  "__assert_fail": ___assert_fail,
  "__cxa_allocate_exception": ___cxa_allocate_exception,
  "__cxa_begin_catch": ___cxa_begin_catch,
